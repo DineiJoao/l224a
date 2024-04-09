ed6ffee822c1ba2435db127b89564b945597b4fa
DineiJoao <146138554+DineiJoao@users.noreply.github.com>
2024-04-09 09:57:44 -0300

    Update fila.c

--- Trabalhos/t2/fila.c
+++ Trabalhos/t2/fila.c
@@ -4,25 +4,35 @@
 #include <stdlib.h>
 #include <string.h>
 
-#define MAX_ELEM 1000
+//#define MAX_ELEM 1000
 
 struct _fila {
   int n_elem;
   int tam_dado;
   int pos_percurso;
+  int *max_elem;
   void *espaco;
 };
 
 // funções que implementam as operações básicas de uma fila
 
 // cria uma fila vazia que suporta dados do tamanho fornecido (em bytes)
-Fila fila_cria(int tam_do_dado) {
-  Fila self = malloc(sizeof(struct _fila));
-  if (self != NULL) {
-    self->espaco = malloc(MAX_ELEM * tam_do_dado);
-    if (self->espaco != NULL) {
-      self->n_elem = 0;
-      self->tam_dado = tam_do_dado;
+Fila *fila_cria(int tam_do_dado) {
+  Fila *self = malloc(sizeof(Fila));
+  if (self!= NULL) {
+    *self = malloc(sizeof(struct _fila));
+    if (*self!= NULL) {
+      (*self)->max_elem = malloc(sizeof(int));
+      *(*self)->max_elem = MAX_ELEM_INICIAL;
+      (*self)->espaco = malloc(*(*self)->max_elem * tam_do_dado);
+      if ((*self)->espaco!= NULL) {
+        (*self)->n_elem = 0;
+        (*self)->tam_dado = tam_do_dado;
+      } else {
+        free((*self)->max_elem);
+        free(*self);
+        *self = NULL;
+      }
     } else {
       free(self);
       self = NULL;
@@ -31,64 +41,66 @@ Fila fila_cria(int tam_do_dado) {
   return self;
 }
 
-void fila_destroi(Fila self) {
-  free(self->espaco);
+void fila_destroi(Fila *self) {
+  free((*self)->espaco);
+  free((*self)->max_elem);
+  free(*self);
   free(self);
 }
 
 // calcula o valor do ponteiro para o elemento na posição pos da fila
-static void *calcula_ponteiro(Fila self, int pos)
+static void *calcula_ponteiro(Fila *self, int pos)
 {
   // TODO: suporte a pos negativa
-  if (pos < 0 || pos >= self->n_elem) return NULL;
+  if (pos < 0 || pos >= (*self)->n_elem) return NULL;
   // calcula a posição convertendo para char *, porque dá para somar em
   //   bytes. tem que fazer essa conversão porque não conhecemos o tipo
   //   do dado do usuário, só o tamanho.
-  void *ptr = (char *)self->espaco + pos * self->tam_dado;
+  void *ptr = (char *)(*self)->espaco + pos * (*self)->tam_dado;
   return ptr;
 }
 
-bool fila_vazia(Fila self) { return self->n_elem == 0; }
+bool fila_vazia(Fila *self) { return (*self)->n_elem == 0; }
 
-void fila_remove(Fila self, void *pdado) {
+void fila_remove(Fila *self, void *pdado) {
   void *ptr = calcula_ponteiro(self, 0);
-  assert(ptr != NULL);
-  if (pdado != NULL) {
-    memmove(pdado, ptr, self->tam_dado);
+  assert(ptr!= NULL);
+  if (pdado!= NULL) {
+    memmove(pdado, ptr, (*self)->tam_dado);
   }
   ptr = calcula_ponteiro(self, 1);
-  if (ptr != NULL) {
+  if (ptr!= NULL) {
     // ptr aponta para o segundo elemento da fila
     //   (que vira o primeiro)
     // copia os dados que sobraram para o início do espaço
     //   (não precisaria fazer isso com vetor circular)
-    memmove(self->espaco, ptr, self->tam_dado * (self->n_elem - 1));
+    memmove((*self)->espaco, ptr, (*self)->tam_dado * ((*self)->n_elem - 1));
   }
-  self->n_elem--;
+  (*self)->n_elem--;
 }
 
-void fila_insere(Fila self, void *pdado) {
-  assert(self->n_elem < MAX_ELEM);
-  self->n_elem++;
-  void *ptr = calcula_ponteiro(self, self->n_elem - 1);
-  memmove(ptr, pdado, self->tam_dado);
+void fila_insere(Fila *self, void *pdado) {
+  assert((*self)->n_elem < *(*self)->max_elem);
+  (*self)->n_elem++;
+  void *ptr = calcula_ponteiro(self, (*self)->n_elem - 1);
+  memmove(ptr, pdado, (*self)->tam_dado);
 }
 
 
-void fila_inicia_percurso(Fila self, int pos_inicial)
+void fila_inicia_percurso(Fila *self, int pos_inicial)
 {
-  self->pos_percurso = pos_inicial;
+  (*self)->pos_percurso = pos_inicial;
 }
 
-bool fila_proximo(Fila self, void *pdado)
+bool fila_proximo(Fila *self, void *pdado)
 {
-  void *ptr = calcula_ponteiro(self, self->pos_percurso);
+  void *ptr = calcula_ponteiro(self, (*self)->pos_percurso);
   if (ptr == NULL) return false;
-  memmove(pdado, ptr, self->tam_dado);
-  if (self->pos_percurso < 0) {
-    self->pos_percurso--;
+  memmove(pdado, ptr, (*self)->tam_dado);
+  if ((*self)->pos_percurso < 0) {
+    (*self)->pos_percurso--;
   } else {
-    self->pos_percurso++;
+    (*self)->pos_percurso++;
   }
   return true;
 }
